480c2e5200087bfa70cd593f15c6196184de5172
Christopher <slimcognitodudesvids@outlook.com>
2019-04-19 07:37:29 +1000

    Added EmbedMethods.cs

---
+++ Frequency2/Methods/EmbedMethods.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using System.Threading.Tasks;
+using Victoria;
+using Victoria.Entities;
+
+namespace Frequency2.Methods
+{
+	public static class EmbedMethods
+	{
+		public static async Task<Embed> GetEmbedQueue(LavaTrack track, LavaPlayer player, LavaTrack ptrack)
+		{
+			EmbedBuilder Embed = new EmbedBuilder
+			{
+				Title = "Playing Song",
+				Description = $"[{track.Title}]({track.Uri.AbsoluteUri})"
+			};
+
+			Embed.AddField("Length", track.Length.ToString(), true);
+			Embed.AddField("Tracks in Queue", (player.Queue.Count).ToString(), true);
+			Embed.AddField("Previous Track", ptrack.Title, true);
+			Embed.AddField("Next Track", (player.Queue.Count == 0) ? "No tracks" : (player.Queue.Peek() as LavaTrack).Title, true);
+
+			Embed.ImageUrl = await track.FetchThumbnailAsync();
+			return Embed.Build();
+		}
+		
+	}
+}
