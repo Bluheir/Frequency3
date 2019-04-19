cf62c366a16ad03e326171f8c7c7ff79270c0b2e
Christopher <slimcognitodudesvids@outlook.com>
2019-04-19 18:06:55 +1000

    Added CommandsReflection.cs for methods relating to CommandInfo

---
+++ Frequency2/Methods/CommandsReflection.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Frequency2.Methods
+{
+	public static class CommandsReflection
+	{
+		public static string GetFullName(CommandInfo Command)
+		=> getfullname(Command, Command.Module);
+		private static string getfullname(CommandInfo command, ModuleInfo module, string f = "")
+		{
+			if(module == null)
+			{
+				return f + command.Name;
+			}
+			if(module.Group == null)
+			{
+				return f + command.Name;
+			}
+			f = module.Group + " ";
+			
+			return f + getfullname(command, module.Parent);
+
+
+		}
+
+		public static CommandInfo GetCommand(string commandname)
+		=>	Frequency2.Source.Frequency2Client.Instance.CommandInfos.GetValueOrDefault(commandname);
+		
+
+		
+	}
+}
