d3e2e728e6a318b530c91422d2206e5e535c8634
Christopher <slimcognitodudesvids@outlook.com>
2019-04-19 18:14:01 +1000

    Added IgnoreAttribute.cs

---
+++ Frequency2/Types/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frequency2.Types.Attributes
+{
+	[System.AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+	public sealed class IgnoreAttribute : Attribute
+	{
+		public IgnoreAttribute()
+		{}
+	}
+}
