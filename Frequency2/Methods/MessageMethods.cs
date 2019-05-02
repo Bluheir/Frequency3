022eb157fac1a76460f0bb4f95b411374ae97285
Christopher <slimcognitodudesvids@outlook.com>
2019-05-03 07:25:41 +1000

    Added a few methods for adding pages to messages sent by the bot

--- Frequency2/Methods/MessageMethods.cs
+++ Frequency2/Methods/MessageMethods.cs
@@ -1,4 +1,8 @@
 ﻿using Discord;
+using Frequency2.Types.Messages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Frequency2.Methods
 {
@@ -13,5 +17,28 @@ namespace Frequency2.Methods
 
 		public static bool IsPrivate(this IMessage message)
 		=> message.Channel is IDMChannel;
+
+		public static async Task PaginateAsync(this IUserMessage message, params Page[] pages)
+		=> await message.PaginateAsync(0, pages);
+
+		public static async Task PaginateAsync(this IUserMessage message, int start, params Page[] pages)
+		=> await PageCollection.PaginateAsync(new PageCollection(pages, message, start));
+
+		public static async Task PaginateAsync(this IUserMessage message, int start, params Embed[] embeds)
+		{
+			List<Page> pages = new List<Page>(embeds.Length);
+			foreach (var item in embeds)
+				pages.Add(new Page(item));
+			await message.PaginateAsync(start, pages.ToArray());
+		}
+
+		public static async Task PaginateAsync(this IUserMessage message, params Embed[] embeds)
+		=> await message.PaginateAsync(0, embeds);
+
+		public static async Task PaginateAsync(this IUserMessage message, int start, params EmbedBuilder[] embeds)
+		=> await message.PaginateAsync(start, embeds.Select(x => x.Build()).ToArray());
+
+		public static async Task PaginateAsync(this IUserMessage message, params EmbedBuilder[] embeds)
+		=> await message.PaginateAsync(0, embeds);
 	}
 }
