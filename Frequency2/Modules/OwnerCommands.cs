7dc8bf246ca18a9ddb83aae036be0b39f94d3e60
Christopher <slimcognitodudesvids@outlook.com>
2019-05-03 07:34:58 +1000

    Added a command to send a test paged message

--- Frequency2/Modules/OwnerCommands.cs
+++ Frequency2/Modules/OwnerCommands.cs
@@ -6,6 +6,9 @@ using System;
 using Discord;
 using static Frequency2.Methods.MessageMethods;
 using Frequency2.Types.Attributes;
+using System.Collections.Generic;
+using Frequency2.Types.Messages;
+using Discord.WebSocket;
 
 namespace Frequency2.Modules
 {
@@ -33,6 +36,50 @@ namespace Frequency2.Modules
 				return;
 			}
 			await Logger.Instance.LogAsync(new LogMessage(LogSeverity.Info, "discordapp.com", message));
+		}
+
+		[Command("testpage")]
+		public async Task TestPaginate()
+		{
+			if (Context.User.Id != Configuration.Config.OwnerId)
+			{
+				await ReplyAsync($"{Context.User.Mention} {GetError(14)}");
+				return;
+			}
+			EmbedBuilder embed1 = new EmbedBuilder
+			{
+				Fields = new List<EmbedFieldBuilder>()
+				{
+					new EmbedFieldBuilder
+					{
+						Name = "hello",
+						Value = "test"
+					},
+					new EmbedFieldBuilder
+					{
+						Name = "hello2",
+						Value = "test2"
+					},
+				}
+			};
+			EmbedBuilder embed2 = new EmbedBuilder
+			{
+				Fields = new List<EmbedFieldBuilder>()
+				{
+					new EmbedFieldBuilder
+					{
+						Name = "hello1234",
+						Value = "test43434"
+					},
+					new EmbedFieldBuilder
+					{
+						Name = "hello212341241234132",
+						Value = "test2132412341241324"
+					},
+				}
+			};
+			var message = await Context.Channel.SendMessageAsync("", false, embed1.Build());
+			await PageCollection.PaginateAsync(new PageCollection(new List<Page> { new Page(embed1.Build()), new Page(embed2.Build()) }, message ));
 		}
 	}
 }
