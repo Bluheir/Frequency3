34a24c7621a2502987a9b9b4ed4c6d9b692efc29
Christopher <slimcognitodudesvids@outlook.com>
2019-05-03 08:06:13 +1000

    Added the Page class to represent a page for a message

---
+++ Frequency2/Types/Messages/Page.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+
+
+namespace Frequency2.Types.Messages
+{
+	public class Page
+	{
+		public Embed Embed { get; }
+		public string Content { get; }
+
+		public Page(Embed embed, string content = "")
+		{
+			Embed = embed;
+			Content = content;
+		}
+	}
+}
