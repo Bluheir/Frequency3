725feba9d92d4549bd0d1e0cc641078f80b7157c
Christopher <slimcognitodudesvids@outlook.com>
2019-05-03 08:05:21 +1000

    Added the INoMessagePageCollection interface to represent a PageCollection without an internal IUserMessage

---
+++ Frequency2/Types/Messages/INoMessagePageCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Frequency2.Types.Messages
+{
+	public interface INoMessagePageCollection : 
+		IEnumerable<Page>,
+		IEnumerable,
+		IReadOnlyList<Page>,
+		IReadOnlyCollection<Page>
+	{
+		IReadOnlyList<Page> Pages { get; }
+		int CurrentPage { get; }
+		new int Count { get; }
+
+	}
+}
