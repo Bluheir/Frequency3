9a581d606c962eea18dec39c691b7150b85432c7
Christopher <slimcognitodudesvids@outlook.com>
2019-09-10 20:48:54 +1000

    Added more properties to GuildMusicConfig

--- Frequency2/Audio/GuildMusicConfig.cs
+++ Frequency2/Audio/GuildMusicConfig.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Victoria.Entities;
 
 namespace Frequency2.Audio
 {
 	public class GuildMusicConfig
 	{
-		public bool Repeat { get; set; } = false;
-		public int? MaxQueueTimes { get; set; } = null;
-		public ConcurrentDictionary<ulong, int> UserQueueTimes { get; set; } = new ConcurrentDictionary<ulong, int>();
-		public bool IsPlayed { get; set; } = false;
+		public bool Repeat { get; set; }
+		public int? MaxQueueTimes { get; set; }
+		public bool IsLocked => MaxQueueTimes == 0;
+		public ConcurrentDictionary<ulong, int> UserQueueTimes { get; }
+		public bool IsPlayed { get; set; }
+		public CQueue<LavaTrack> Queue { get; }
+
+		public GuildMusicConfig()
+		{
+			Repeat = false;
+			MaxQueueTimes = null;
+			UserQueueTimes = new ConcurrentDictionary<ulong, int>();
+			IsPlayed = false;
+			Queue = new CQueue<LavaTrack>();
+		}
 	}
 }
