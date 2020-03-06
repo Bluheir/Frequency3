929c68e8cb6f9bb5eb8846eda787a42a9291c6b1
Christopher <slimcognitodudesvids@outlook.com>
2020-03-06 16:20:00 +1100

    Added TrackInput.cs to represent a single item in a music queue

---
+++ Frequency3/Audio/TrackInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Victoria;
+
+namespace Frequency3.Audio
+{
+	public class TrackInput
+	{
+		public LavaTrack Track { get; }
+		public ulong UserInputter { get; }
+
+		public TrackInput(LavaTrack track, ulong user)
+		{
+			Track = track ?? throw new ArgumentNullException($"Argument {nameof(track)} cannot be null.");\
+			UserInputter = user;
+		}
+
+		public static implicit operator LavaTrack(TrackInput input)
+		{
+			return input.Track;
+		}
+	}
+}
