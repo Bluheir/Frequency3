a42f7a6fd05a8442d0fd5f1622d8f5b5d9ccfb86
Christopher <slimcognitodudesvids@outlook.com>
2020-03-06 16:19:30 +1100

    Added a music config for guilds

---
+++ Frequency3/Audio/GuildMusicConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+using Victoria;
+
+namespace Frequency3.Audio
+{
+	public class GuildMusicConfig
+	{
+		public bool Repeat { get; set; }
+		public int? MaxQueueTimes { get; set; }
+		public bool IsLocked => MaxQueueTimes == 0;
+		public ConcurrentDictionary<ulong, int> UserQueueTimes { get; }
+		public bool IsPlayed { get; set; }
+		public CQueue<TrackInput> Queue { get; }
+
+		public GuildMusicConfig()
+		{
+			Repeat = false;
+			MaxQueueTimes = null;
+			UserQueueTimes = new ConcurrentDictionary<ulong, int>();
+			IsPlayed = false;
+			Queue = new CQueue<TrackInput>();
+		}
+		public int GetUserInputs(ulong id)
+		{
+			return UserQueueTimes.GetOrAdd(id, x => 0);
+		}
+		public int DecrementUser(ulong id)
+		{
+			return UserQueueTimes.AddOrUpdate(id, 0, (x, y) =>
+			{
+				if (y == 0)
+					return 0;
+				return y - 1;
+			});
+		}
+	}
+}
