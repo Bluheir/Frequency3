b16cd9dded1bbcf2a6b718c2bf69980cf7de86b1
Christopher <slimcognitodudesvids@outlook.com>
2020-03-06 16:21:27 +1100

    Added AudioModule to represent the commands for audio and playing audio

---
+++ Frequency3/Modules/AudioModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Frequency3.Audio;
+using Discord;
+
+namespace Frequency3.Modules
+{
+	public class AudioModule : ModuleBase<ShardedCommandContext>
+	{
+		private static AudioService? _audio => AudioService.Instance;
+
+		[Command("play")]
+		public async Task PlaySongAsync([Remainder]string song)
+		{
+			try
+			{
+				await _audio.PlayTrackAsync(song, Context, Context.Channel as ITextChannel, false);
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+		[Command("queue")]
+		public async Task EnqueueAsync([Remainder]string song)
+		{
+			await _audio.PlayTrackAsync(song, Context, Context.Channel as ITextChannel, false);
+		}
+	}
+}
